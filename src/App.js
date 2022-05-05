f0804f84f96cf801ddbfcdae1e2b0eb14aa01921
Pawel Kowalewski <pkowalewski@microstrategy.com>
2022-05-05 23:15:58 +0300

    checkWallet function

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
-import React from 'react';
-import './styles/App.css';
-import twitterLogo from './assets/twitter-logo.svg';
-
-// Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+import React, { useEffect, useState } from "react";
+import "./styles/App.css";
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
 
-  return (
-		<div className="App">
-			<div className="container">
+  const checkIfWalletIsConnected = () => {
+    const { ethereum } = window;
 
-				<div className="header-container">
-					<header>
-            <div className="left">
-              <p className="title">🐱‍👤 Ninja Name Service</p>
-              <p className="subtitle">Your immortal API on the blockchain!</p>
-            </div>
-					</header>
-				</div>
+    if (!ethereum) {
+      console.log("Make sure you have MetaMask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  };
 
-        <div className="footer-container">
-					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-					<a
-						className="footer-text"
-						href={TWITTER_LINK}
-						target="_blank"
-						rel="noreferrer"
-					>{`built with @${TWITTER_HANDLE}`}</a>
-				</div>
-			</div>
-		</div>
-	);
-}
+  const renderNotConnectedContainer = () => (
+    <div className="connect-wallet-container">
+      <button className="cta-button connect-wallet-button">
+        Connect Wallet
+      </button>
+    </div>
+  );
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  return <div className="App">{renderNotConnectedContainer()}</div>;
+};
 
 export default App;
